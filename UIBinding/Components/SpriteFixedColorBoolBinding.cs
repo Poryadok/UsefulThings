504a72c3caddc6bfd5c4801615196826542e0a6f
Poryadok <ChronosVellior@yandex.ru>
2020-09-10 00:17:11 +0300

    SpriteFixedColorBoolBinding

---
+++ UIBinding/Components/SpriteFixedColorBoolBinding.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UIBinding.Base;
+
+namespace UIBinding.Components
+{
+	[RequireComponent(typeof(SpriteRenderer))]
+	public class SpriteFixedColorBoolBinding : BaseBinding<BoolProperty>
+	{
+		[SerializeField]
+		private Color m_colorOnTrue = Color.white;
+		[SerializeField]
+		private Color m_colorOnFalse = Color.white;
+
+		private SpriteRenderer spriteRenderer;
+
+		private void Awake()
+		{
+			spriteRenderer = GetComponent<SpriteRenderer>();
+		}
+
+		protected override void OnUpdateValue()
+		{
+			if (property.value)
+			{
+				spriteRenderer.color = m_colorOnTrue;
+			}
+			else
+			{
+				spriteRenderer.color = m_colorOnFalse;
+			}
+		}
+	}
+}
+
