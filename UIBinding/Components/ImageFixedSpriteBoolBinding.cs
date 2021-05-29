43b0dfcf8ed89b6c6f38f94b0b8f0d19ff21bf1f
Poryadok <31483324+Poryadok@users.noreply.github.com>
2021-05-29 23:30:09 +0300

    new bindings

---
+++ UIBinding/Components/ImageFixedSpriteBoolBinding.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using UIBinding.Base;
+
+namespace UIBinding.Components
+{
+	[RequireComponent(typeof(Image))]
+	public class ImageFixedSpriteBoolBinding : BaseBinding<BoolProperty>
+	{
+		[SerializeField]
+		private Sprite imageOnTrue = null;
+		[SerializeField]
+		private Sprite imageOnFalse = null;
+
+		private Image image;
+
+		private void Awake()
+		{
+			image = GetComponent<Image>();
+		}
+
+		protected override void OnUpdateValue()
+		{
+			if (property.value)
+			{
+				image.sprite = imageOnTrue;
+			}
+			else
+			{
+				image.sprite = imageOnFalse;
+			}
+		}
+	}
+}
+
---
+++ UIBinding/Components/ImageFixedSpriteIntBinding.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+using UIBinding.Base;
+
+namespace UIBinding.Components
+{
+	[RequireComponent(typeof(Image))]
+	public class ImageFixedSpriteIntBinding : BaseBinding<IntProperty>
+	{
+		[Serializable]
+		public class SpriteElement
+		{
+			public int value { get { return m_value; } }
+			public Sprite sprite { get { return _sprite; } }
+
+			[SerializeField]
+			private int m_value = 0;
+			[SerializeField]
+			private Sprite _sprite = null;
+		}
+
+		[SerializeField]
+		private SpriteElement[] m_elements = null;
+
+		private Image m_graphic;
+
+		private void Awake()
+		{
+			m_graphic = GetComponent<Image>();
+		}
+
+		protected override void OnUpdateValue()
+		{
+			var value = property.value;
+			for (int i = 0; i < m_elements.Length; i++)
+			{
+				var element = m_elements[i];
+				if (element.value == value)
+				{
+					m_graphic.sprite = element.sprite;
+					break;
+				}
+			}
+		}
+	}
+}
+
