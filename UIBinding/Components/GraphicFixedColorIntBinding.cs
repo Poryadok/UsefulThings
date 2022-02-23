d4f29c79041dd0990cbfb505054079d0aa12a829
Sergey <sergey.skornyakov@melscience.com>
2022-02-23 16:54:40 +0300

    better fixed color int bindings

--- UIBinding/Components/GraphicFixedColorIntBinding.cs
+++ UIBinding/Components/GraphicFixedColorIntBinding.cs
@@ -3,46 +3,124 @@ using UnityEngine.UI;
 using System;
 
 using UIBinding.Base;
+using System.Collections.Generic;
 
 namespace UIBinding.Components
 {
 	[RequireComponent(typeof(Graphic))]
 	public class GraphicFixedColorIntBinding : BaseBinding<IntProperty>
 	{
-		[Serializable]
-		public class ColorElement
+		public enum CheckType
 		{
-			public int value { get { return m_value; } }
-			public Color color { get { return m_color; } }
-
-			[SerializeField]
-			private int m_value = 0;
-			[SerializeField]
-			private Color m_color = Color.white;
+			Equal,
+			Greater,
+			Less,
+			InRange
 		}
 
 		[SerializeField]
-		private ColorElement[] m_elements = null;
+		private CheckType m_checkType = CheckType.Equal;
+		[SerializeField]
+		private string m_value = "0";
+		[SerializeField]
+		private bool m_invert = false;
+		[SerializeField]
+		private Color m_colorTrue = Color.white;
+		[SerializeField]
+		private Color m_colorFalse = Color.white;
+
+		private List<int> m_values = new List<int>();
 
 		private Graphic m_graphic;
 
 		private void Awake()
 		{
 			m_graphic = GetComponent<Graphic>();
+			ParseValues();
 		}
+
+		private void ParseValues()
+		{
+			m_values.Clear();
+
+			switch (m_checkType) {
+				case CheckType.Equal: {
+						var trimmedValue = m_value.Trim();
+						var stringValues = trimmedValue.Split(',');
+						foreach (var stringValue in stringValues) {
+							AddValue(stringValue);
+						}
+					}
+					break;
+				case CheckType.Greater:
+				case CheckType.Less: {
+						AddValue(m_value);
+					}
+					break;
+				case CheckType.InRange: {
+						var trimmedValue = m_value.Trim();
+						var stringValues = trimmedValue.Split(',');
+						if (stringValues.Length < 2) {
+#if UNITY_EDITOR
+							Debug.LogErrorFormat("Couldn't parse \"{0}\" to \"{1}\" check type", m_value, m_checkType.ToString());
+#endif
+							return;
+						}
+						for (int i = 0; i < 2; i++) {
+							var stringValue = stringValues[i];
+							AddValue(stringValue);
+						}
+					}
+					break;
+			}
+		}
+
+		private void AddValue(string stringValue)
+		{
+			var value = 0;
+			if (int.TryParse(stringValue, out value)) {
+				m_values.Add(value);
+			}
+#if UNITY_EDITOR
+			else {
+				Debug.LogErrorFormat("Couldn't parse \"{0}\" to {1} type", m_value, m_checkType.ToString());
+			}
+#endif
+		}
+
 
 		protected override void OnUpdateValue()
 		{
-			var value = property.value;
-			for (int i = 0; i < m_elements.Length; i++)
-			{
-				var element = m_elements[i];
-				if (element.value == value)
-				{
-					m_graphic.color = element.color;
+			var result = false;
+			switch (m_checkType) {
+				case CheckType.Equal: {
+						var propertyValue = property.value;
+						foreach (var value in m_values) {
+							if (propertyValue == value) {
+								result = true;
+								break;
+							}
+						}
+					}
 					break;
-				}
+				case CheckType.Greater: {
+						var value = m_values[0];
+						result = property.value > value;
+					}
+					break;
+				case CheckType.Less: {
+						var value = m_values[0];
+						result = property.value < value;
+					}
+					break;
+				case CheckType.InRange: {
+						var minValue = m_values[0];
+						var maxValue = m_values[1];
+						result = (property.value >= minValue) && (property.value <= maxValue);
+					}
+					break;
 			}
+			m_graphic.color = result ? m_colorTrue : m_colorFalse;
 		}
 	}
 }
