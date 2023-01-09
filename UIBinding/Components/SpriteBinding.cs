834ab76b395b82f102eab82f309ec10c791335b5
PlushMinush <ChronosVellior@yandex.ru>
2023-01-09 10:06:59 +0300

    spritebinding

---
+++ UIBinding/Components/SpriteBinding.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using UIBinding.Base;
+
+namespace UIBinding.Components
+{
+	[RequireComponent(typeof(SpriteRenderer))]
+	public class SpriteBinding : BaseBinding<SpriteProperty>
+	{
+		private SpriteRenderer m_component;
+
+		private void Awake()
+		{
+			m_component = GetComponent<SpriteRenderer>();
+		}
+
+		protected override void OnUpdateValue()
+		{
+			m_component.sprite = property.value;
+		}
+	}
+}
