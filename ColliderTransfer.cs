3789a897da0a21892b73a5a348f507f1141170c0
Poryadok <ChronosVellior@yandex.ru>
2024-02-23 16:44:21 +0300

    ColliderTransfer sends more events

--- ColliderTransfer.cs
+++ ColliderTransfer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,35 @@ namespace PM.UsefulThings
 	public class ColliderTransfer : MonoBehaviour
 	{
 		public event System.Action OnMouseClick;
+		public event System.Action OnMousePressed;
+		public event System.Action OnMouseReleased;
+
+		private bool isMouseOver;
+
+		private void OnMouseOver()
+		{
+			isMouseOver = true;
+		}
+
+		private void OnMouseExit()
+		{
+			isMouseOver = false;
+		}
 
 		private void OnMouseUpAsButton()
 		{
 			OnMouseClick?.Invoke();
+		}
+		
+		private void OnMouseDown()
+		{
+			OnMousePressed?.Invoke();
+		}
+		
+		private void OnMouseUp()
+		{
+			if (isMouseOver)
+				OnMouseReleased?.Invoke();
 		}
 	}
 }
